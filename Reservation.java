47e094943a05741c2fa3353d2df1796d211aa4e1
Omar Zamel <omar.zamel5@gmail.com>
2024-12-06 19:21:18 +0200

    Reservation

--- Reservation.java
+++ Reservation.java
@@ -19,6 +19,69 @@ public class Reservation {
         this.slotID = slotID;
         this.status = true;
         this.reservationDate = LocalDate.now();
+    }
 
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public int getSlotID() {
+        return slotID;
+    }
+
+    public void setSlotID(int slotID) {
+        this.slotID = slotID;
+    }
+
+    public LocalDate getReservationDate() {
+        return reservationDate;
+    }
+
+    public void setReservationDate(LocalDate reservationDate) {
+        this.reservationDate = reservationDate;
+    }
+
+    public int getSpotID() {
+        return spotID;
+    }
+
+    public void setSpotID(int spotID) {
+        this.spotID = spotID;
+    }
+
+    public int getResID() {
+        return resID;
+    }
+
+    public void setResID(int resID) {
+        this.resID = resID;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+
+    public String getOwnerID() {
+        return ownerID;
+    }
+
+    public void setOwnerID(String ownerID) {
+        this.ownerID = ownerID;
+    }
+
+    public static int getReservationID() {
+        return reservationID;
+    }
+
+    public static void setReservationID(int reservationID) {
+        Reservation.reservationID = reservationID;
     }
 }
