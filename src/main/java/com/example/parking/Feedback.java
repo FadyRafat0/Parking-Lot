43a75880eb232a5d025b18eeb787729450490837
Bouthaina Yasser <86330189+bousyyasser@users.noreply.github.com>
2024-12-10 16:52:06 +0200

    Add files via upload

--- src/main/java/com/example/parking/Feedback.java
+++ src/main/java/com/example/parking/Feedback.java
@@ -6,23 +6,24 @@ import java.io.PrintStream;
 
 public class Feedback
 {
-    private int rate;
+    private double rate;
     private String feedbackMessage;
     private final int ownerID;
     private final int ReservationID;
 
-    public Feedback(int ownerID, int ReservationID)
+    public Feedback(int ownerID, int ReservationID,double rate, String feedbackMessage)
     {
-        IDsCheck(ownerID,ReservationID);
         this.ownerID=ownerID;
         this.ReservationID=ReservationID;
+        this.rate=rate;
+        this.feedbackMessage=feedbackMessage;
     }
 
     public void setFeedback(int rate, String feedbackMessage)
     {
         this.rate=rate;
         this.feedbackMessage=feedbackMessage;
-        saveFeedbackToFile();
+        //saveFeedbackToFile();
 
 
     }
@@ -33,7 +34,7 @@ public class Feedback
         System.out.println("Rating: "+rate+" out of 5");
         System.out.println("Feedback comment: "+ feedbackMessage);
     }
-    public int getRate() {
+    public double getRate() {
         return rate;
     }
 
@@ -49,42 +50,35 @@ public class Feedback
         return ReservationID;
     }
 
-    private void IDsCheck(int ownerID, int ReservationID)
-    {
-        if(ownerID<1 || ReservationID<1)
-        {
-            throw  new IllegalArgumentException("Invalid ID Number! Please try again");
-        }
-    }
 
 
-    private void saveFeedbackToFile() {
-        String fileName = "feedbacks.txt";
-        FileOutputStream myFile = null;
-        PrintStream print =null;
-        try{
-           myFile= new FileOutputStream(fileName,true);
-           print = new PrintStream(myFile);
-            print.println("Owner ID: "+ownerID + " ,Reservation ID: "+ ReservationID);
-            print.println("Rating: "+ rate +" Out of 5");
-            print.println("Feedback comment: "+ feedbackMessage);
-            System.out.println("========================================");
-
-        }catch (IOException e){
-            System.out.println("Error saving feedback to file: " + e.getMessage());
-
-        }
-        finally {
-
-               try {
-                   if(myFile!= null)
-                      myFile.close();
-                   if (print != null)
-                       print.close();
-               }catch(Exception ex){
-                   System.out.println("Error closing file, "+ex.getMessage());
-            }
-        }
-
-    }
+//    private void saveFeedbackToFile() {
+//        String fileName = "feedbacks.txt";
+//        FileOutputStream myFile = null;
+//        PrintStream print =null;
+//        try{
+//           myFile= new FileOutputStream(fileName,true);
+//           print = new PrintStream(myFile);
+//            print.println("Owner ID: "+ownerID + " ,Reservation ID: "+ ReservationID);
+//            print.println("Rating: "+ rate +" Out of 5");
+//            print.println("Feedback comment: "+ feedbackMessage);
+//            System.out.println("========================================");
+//
+//        }catch (IOException e){
+//            System.out.println("Error saving feedback to file: " + e.getMessage());
+//
+//        }
+//        finally {
+//
+//               try {
+//                   if(myFile!= null)
+//                      myFile.close();
+//                   if (print != null)
+//                       print.close();
+//               }catch(Exception ex){
+//                   System.out.println("Error closing file, "+ex.getMessage());
+//            }
+//        }
+//
+//    }
 }
